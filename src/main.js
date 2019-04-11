6731dde066ded69bdff74d0cab7c60978c5766f1
Lyle Reed <lyleareed@gmail.com>
2019-04-11 15:17:36 -0500

    Commit vue standard init files

--- src/main.js
+++ src/main.js
@@ -1,38 +1,10 @@
-import Vue from "vue";
-import Vuex from "vuex";
-// import Router from "vue-router";
-import App from "./App";
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
 
-Vue.config.productionTip = false;
-Vue.use(Vuex);
-// Vue.use(Router);
+Vue.config.productionTip = false
 
-// const router = new Router({
-//   routes: [
-//     {
-//       path: "/",
-//       name: "home",
-//       component: home
-//     }
-//   ]
-// });
-
-const store = new Vuex.Store({
-  state: {
-    cash: 0
-  },
-  mutations: {
-    addSheet(state, sheet) {
-      state.sheets.push(sheet);
-    }
-  }
-});
-
-/* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  components: { App },
-  // router,
   store,
-  template: "<App/>"
-});
+  render: h => h(App)
+}).$mount('#app')
